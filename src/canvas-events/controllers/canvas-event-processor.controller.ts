cd443ba6756d66c0f36c41b838a71a0ec8d76b01
kytas999 <kytasevichvlad@gmail.com>
2025-08-24 23:43:45 +0300

    feat(canvas-events): enhance logging for canvas webhook processing

--- src/canvas-events/controllers/canvas-event-processor.controller.ts
+++ src/canvas-events/controllers/canvas-event-processor.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Logger, Post } from '@nestjs/common';
+import { Body, Controller, Logger, Post, Req } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 import { CanvasDomain } from '../decorators/canvas-domain.decorator';
 import { CanvasEventDto } from '../dto';
 import { CanvasEventProcessorService } from '../services/canvas-event-processor.service';
+import { url } from 'inspector';
 
 @ApiTags('Canvas Event Processor')
 @Controller('canvas-events')
@@ -18,10 +19,19 @@ export class CanvasEventProcessorController {
   processCanvasWebhook(
     @Body() event: CanvasEventDto,
     @CanvasDomain() domain: string,
+    @Req() request: Request,
   ) {
     this.logger.log(
       `Processing canvas webhook for domain: ${domain}`,
-      JSON.stringify(event, null, 2),
+      JSON.stringify(
+        {
+          headers: request.headers,
+          body: event,
+          url: request.url,
+        },
+        null,
+        2,
+      ),
     );
     return this.canvasEventProcessorService.processCanvasEvent(event, domain);
   }
