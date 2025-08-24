314d54ca5854abe9c312bb221e8e4c6591d29c1c
kytas999 <kytasevichvlad@gmail.com>
2025-08-25 00:18:40 +0300

    feat(swagger): update swagger setup and remove outdated routes from vercel config

--- src/core/config/swagger.setup.ts
+++ src/core/config/swagger.setup.ts
@@ -1,5 +1,9 @@
 import { INestApplication } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import {
+  DocumentBuilder,
+  SwaggerCustomOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 
 import { SWAGGER_API_MODELS } from './swagger.models';
 
@@ -17,11 +21,19 @@ export function setupSwagger(app: INestApplication) {
   });
 
   // Custom Swagger options for production compatibility
-  const swaggerOptions = {
+  const swaggerOptions: SwaggerCustomOptions = {
     swaggerOptions: {
       persistAuthorization: true,
     },
     customSiteTitle: 'ECAP API Documentation',
+    customfavIcon:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/favicon.ico',
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/swagger-ui-standalone-preset.min.js',
+    ],
   };
 
   SwaggerModule.setup('api/docs', app, document, swaggerOptions);
