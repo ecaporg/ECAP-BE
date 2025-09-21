3285bc03daf168fc3a26c59920d6d6356cd527a1
kytas999 <kytasevichvlad@gmail.com>
2025-09-21 18:22:59 +0300

    fix: comment out unused cache-related code and dependencies

--- src/core/decorators/cache.decorator.ts
+++ src/core/decorators/cache.decorator.ts
@@ -1,12 +1,12 @@
-import { ExecutionContext, SetMetadata, UseInterceptors } from '@nestjs/common';
+// import { ExecutionContext, SetMetadata, UseInterceptors } from '@nestjs/common';
 
-import { CACHE_KEY_METADATA, CACHE_TTL_METADATA } from '../constants/cache';
-import { RoleCacheInterceptor } from '../interceptors/role-cache.interceptor';
+// import { CACHE_KEY_METADATA, CACHE_TTL_METADATA } from '../constants/cache';
+// import { RoleCacheInterceptor } from '../interceptors/role-cache.interceptor';
 
-export const CacheKey = (key: string) => SetMetadata(CACHE_KEY_METADATA, key);
+// export const CacheKey = (key: string) => SetMetadata(CACHE_KEY_METADATA, key);
 
-type CacheTTLFactory = (ctx: ExecutionContext) => Promise<number> | number;
-export const CacheTTL = (ttl: number | CacheTTLFactory) =>
-  SetMetadata(CACHE_TTL_METADATA, ttl);
+// type CacheTTLFactory = (ctx: ExecutionContext) => Promise<number> | number;
+// export const CacheTTL = (ttl: number | CacheTTLFactory) =>
+//   SetMetadata(CACHE_TTL_METADATA, ttl);
 
-export const RoleCache = () => UseInterceptors(RoleCacheInterceptor);
+// export const RoleCache = () => UseInterceptors(RoleCacheInterceptor);
