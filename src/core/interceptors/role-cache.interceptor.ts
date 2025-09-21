4f87447a97357ea9abc7d4b3cd0b8b06f873764c
kytas999 <kytasevichvlad@gmail.com>
2025-09-21 18:24:58 +0300

    fix: comment out unused cache interceptor code

--- src/core/interceptors/role-cache.interceptor.ts
+++ src/core/interceptors/role-cache.interceptor.ts
@@ -1,139 +1,139 @@
-import { Observable, of } from 'rxjs';
-import { tap } from 'rxjs/operators';
+// import { Observable, of } from 'rxjs';
+// import { tap } from 'rxjs/operators';
 
-import {
-  CallHandler,
-  ExecutionContext,
-  Inject,
-  Injectable,
-  Logger,
-  NestInterceptor,
-  Optional,
-  StreamableFile,
-} from '@nestjs/common';
-import { isFunction, isNil } from '@nestjs/common/utils/shared.utils';
-import { HttpAdapterHost, Reflector } from '@nestjs/core';
+// import {
+//   CallHandler,
+//   ExecutionContext,
+//   Inject,
+//   Injectable,
+//   Logger,
+//   NestInterceptor,
+//   Optional,
+//   StreamableFile,
+// } from '@nestjs/common';
+// import { isFunction, isNil } from '@nestjs/common/utils/shared.utils';
+// import { HttpAdapterHost, Reflector } from '@nestjs/core';
 
-import {
-  CACHE_KEY_METADATA,
-  CACHE_MANAGER,
-  CACHE_TTL_METADATA,
-} from '../constants/cache';
+// import {
+//   CACHE_KEY_METADATA,
+//   CACHE_MANAGER,
+//   CACHE_TTL_METADATA,
+// } from '../constants/cache';
 
-@Injectable()
-export class CacheInterceptor implements NestInterceptor {
-  @Optional()
-  @Inject()
-  protected readonly httpAdapterHost: HttpAdapterHost;
+// @Injectable()
+// export class CacheInterceptor implements NestInterceptor {
+//   @Optional()
+//   @Inject()
+//   protected readonly httpAdapterHost: HttpAdapterHost;
 
-  protected allowedMethods = ['GET'];
+//   protected allowedMethods = ['GET'];
 
-  constructor(
-    @Inject(CACHE_MANAGER) protected readonly cacheManager: any,
-    protected readonly reflector: Reflector,
-  ) {}
+//   constructor(
+//     @Inject(CACHE_MANAGER) protected readonly cacheManager: any,
+//     protected readonly reflector: Reflector,
+//   ) {}
 
-  async intercept(
-    context: ExecutionContext,
-    next: CallHandler,
-  ): Promise<Observable<any>> {
-    const key = this.trackBy(context);
-    const ttlValueOrFactory =
-      this.reflector.get(CACHE_TTL_METADATA, context.getHandler()) ??
-      this.reflector.get(CACHE_TTL_METADATA, context.getClass()) ??
-      null;
+//   async intercept(
+//     context: ExecutionContext,
+//     next: CallHandler,
+//   ): Promise<Observable<any>> {
+//     const key = this.trackBy(context);
+//     const ttlValueOrFactory =
+//       this.reflector.get(CACHE_TTL_METADATA, context.getHandler()) ??
+//       this.reflector.get(CACHE_TTL_METADATA, context.getClass()) ??
+//       null;
 
-    if (!key) {
-      return next.handle();
-    }
-    try {
-      const value = await this.cacheManager.get(key);
-      this.setHeadersWhenHttp(context, value);
+//     if (!key) {
+//       return next.handle();
+//     }
+//     try {
+//       const value = await this.cacheManager.get(key);
+//       this.setHeadersWhenHttp(context, value);
 
-      if (!isNil(value)) {
-        return of(value);
-      }
-      const ttl = isFunction(ttlValueOrFactory)
-        ? await ttlValueOrFactory(context)
-        : ttlValueOrFactory;
+//       if (!isNil(value)) {
+//         return of(value);
+//       }
+//       const ttl = isFunction(ttlValueOrFactory)
+//         ? await ttlValueOrFactory(context)
+//         : ttlValueOrFactory;
 
-      return next.handle().pipe(
-        tap(async (response) => {
-          if (response instanceof StreamableFile) {
-            return;
-          }
+//       return next.handle().pipe(
+//         tap(async (response) => {
+//           if (response instanceof StreamableFile) {
+//             return;
+//           }
 
-          const args = [key, response];
-          if (!isNil(ttl)) {
-            args.push(ttl);
-          }
+//           const args = [key, response];
+//           if (!isNil(ttl)) {
+//             args.push(ttl);
+//           }
 
-          try {
-            await this.cacheManager.set(...args);
-          } catch (err) {
-            Logger.error(
-              `An error has occurred when inserting "key: ${key}", "value: ${response}"`,
-              err.stack,
-              'CacheInterceptor',
-            );
-          }
-        }),
-      );
-    } catch {
-      return next.handle();
-    }
-  }
+//           try {
+//             await this.cacheManager.set(...args);
+//           } catch (err) {
+//             Logger.error(
+//               `An error has occurred when inserting "key: ${key}", "value: ${response}"`,
+//               err.stack,
+//               'CacheInterceptor',
+//             );
+//           }
+//         }),
+//       );
+//     } catch {
+//       return next.handle();
+//     }
+//   }
 
-  protected trackBy(context: ExecutionContext): string | undefined {
-    const httpAdapter = this.httpAdapterHost.httpAdapter;
-    const isHttpApp = httpAdapter && !!httpAdapter.getRequestMethod;
-    const cacheMetadata = this.reflector.get(
-      CACHE_KEY_METADATA,
-      context.getHandler(),
-    );
+//   protected trackBy(context: ExecutionContext): string | undefined {
+//     const httpAdapter = this.httpAdapterHost.httpAdapter;
+//     const isHttpApp = httpAdapter && !!httpAdapter.getRequestMethod;
+//     const cacheMetadata = this.reflector.get(
+//       CACHE_KEY_METADATA,
+//       context.getHandler(),
+//     );
 
-    if (!isHttpApp || cacheMetadata) {
-      return cacheMetadata;
-    }
+//     if (!isHttpApp || cacheMetadata) {
+//       return cacheMetadata;
+//     }
 
-    const request = context.getArgByIndex(0);
-    if (!this.isRequestCacheable(context)) {
-      return undefined;
-    }
-    return httpAdapter.getRequestUrl(request);
-  }
+//     const request = context.getArgByIndex(0);
+//     if (!this.isRequestCacheable(context)) {
+//       return undefined;
+//     }
+//     return httpAdapter.getRequestUrl(request);
+//   }
 
-  protected isRequestCacheable(context: ExecutionContext): boolean {
-    const req = context.switchToHttp().getRequest();
-    return this.allowedMethods.includes(req.method);
-  }
+//   protected isRequestCacheable(context: ExecutionContext): boolean {
+//     const req = context.switchToHttp().getRequest();
+//     return this.allowedMethods.includes(req.method);
+//   }
 
-  protected setHeadersWhenHttp(context: ExecutionContext, value: any): void {
-    if (!this.httpAdapterHost) {
-      return;
-    }
-    const { httpAdapter } = this.httpAdapterHost;
-    if (!httpAdapter) {
-      return;
-    }
-    const response = context.switchToHttp().getResponse();
-    httpAdapter.setHeader(response, 'X-Cache', isNil(value) ? 'MISS' : 'HIT');
-  }
-}
+//   protected setHeadersWhenHttp(context: ExecutionContext, value: any): void {
+//     if (!this.httpAdapterHost) {
+//       return;
+//     }
+//     const { httpAdapter } = this.httpAdapterHost;
+//     if (!httpAdapter) {
+//       return;
+//     }
+//     const response = context.switchToHttp().getResponse();
+//     httpAdapter.setHeader(response, 'X-Cache', isNil(value) ? 'MISS' : 'HIT');
+//   }
+// }
 
-@Injectable()
-export class RoleCacheInterceptor extends CacheInterceptor {
-  constructor(
-    @Inject(CACHE_MANAGER) cacheManager: Cache,
-    reflector: Reflector,
-  ) {
-    super(cacheManager, reflector);
-  }
+// @Injectable()
+// export class RoleCacheInterceptor extends CacheInterceptor {
+//   constructor(
+//     @Inject(CACHE_MANAGER) cacheManager: Cache,
+//     reflector: Reflector,
+//   ) {
+//     super(cacheManager, reflector);
+//   }
 
-  protected trackBy(context: ExecutionContext): string | undefined {
-    const request = context.switchToHttp().getRequest();
-    const userId = request.user?.id;
-    const res = super.trackBy(context);
-    return userId && res ? `${res}:userId=${userId}` : res;
-  }
-}
+//   protected trackBy(context: ExecutionContext): string | undefined {
+//     const request = context.switchToHttp().getRequest();
+//     const userId = request.user?.id;
+//     const res = super.trackBy(context);
+//     return userId && res ? `${res}:userId=${userId}` : res;
+//   }
+// }
