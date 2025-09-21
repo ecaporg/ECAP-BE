8629fe33c7703d9882f81ce83308571225e6ec2c
kytas999 <kytasevichvlad@gmail.com>
2025-09-21 22:50:03 +0300

    Revert "Revert "Revert "feat: add cache manager dependencies and implement Redis caching module"""
    This reverts commit 7b39075f46514add0cf2100c730b4e39917b89ee.

--- src/core/decorators/cache.decorator.ts
+++ src/core/decorators/cache.decorator.ts
@@ -1,12 +1,4 @@
-// import { ExecutionContext, SetMetadata, UseInterceptors } from '@nestjs/common';
+// import { UseInterceptors } from '@nestjs/common';
 
-// import { CACHE_KEY_METADATA, CACHE_TTL_METADATA } from '../constants/cache';
 // import { RoleCacheInterceptor } from '../interceptors/role-cache.interceptor';
-
-// export const CacheKey = (key: string) => SetMetadata(CACHE_KEY_METADATA, key);
-
-// type CacheTTLFactory = (ctx: ExecutionContext) => Promise<number> | number;
-// export const CacheTTL = (ttl: number | CacheTTLFactory) =>
-//   SetMetadata(CACHE_TTL_METADATA, ttl);
-
 // export const RoleCache = () => UseInterceptors(RoleCacheInterceptor);
