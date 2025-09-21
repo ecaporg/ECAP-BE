6e4ff126201a6dffe171f994f5464a8f9c8af154
kytas999 <kytasevichvlad@gmail.com>
2025-09-21 19:09:27 +0300

    copilot fix

--- api/index.ts
+++ api/index.ts
@@ -1,26 +1,37 @@
-import express from 'express';
+// Інші імпорти
+import { useContainer } from 'class-validator';
 
+// import { DataSource } from 'typeorm';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 import { AppModule } from '../src/app.module';
+import { AuthModule } from '../src/auth/auth.module';
+import { setupSwagger } from '../src/core/config/swagger.setup';
 
-const expressServer = express();
-let app: any;
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger:
+      process.env.NODE_ENV === 'production' ? ['error', 'warn'] : undefined,
+  });
 
-async function createNestApp() {
-  if (!app) {
-    app = await NestFactory.create(
-      AppModule,
-      new ExpressAdapter(expressServer),
-    );
-    app.setGlobalPrefix('api');
-    await app.init();
-  }
-  return expressServer;
+  app.setGlobalPrefix('api');
+
+  setupSwagger(app);
+
+  useContainer(app.select(AuthModule), { fallbackOnErrors: true });
+
+  const configService = app.get(ConfigService);
+
+  const corsOptions: CorsOptions = {
+    origin: ['http://localhost:3000', configService.get('FRONTEND_URL')],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
+
+  app.enableCors(corsOptions);
+  await app.listen(configService.get('PORT'));
 }
-
-export default async (req: any, res: any) => {
-  await createNestApp();
-  expressServer(req, res);
-};
+bootstrap();
