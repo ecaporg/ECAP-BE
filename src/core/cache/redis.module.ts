74f5a175428a47de2ba6a3b0b03fd187502d1440
kytas999 <kytasevichvlad@gmail.com>
2025-09-21 23:15:49 +0300

    Revert "2"
    This reverts commit 0a3251ebb43c4f6451cf46055f8a8976543dc879.

--- src/core/cache/redis.module.ts
+++ src/core/cache/redis.module.ts
@@ -1,33 +1,33 @@
 // import { redisStore } from 'cache-manager-redis-yet';
-// import { CacheModule } from '@nestjs/cache-manager';
-// import { Module } from '@nestjs/common';
-// import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
-// @Module({
-//   imports: [
-//     CacheModule.registerAsync({
-//       imports: [ConfigModule],
-//       useFactory: async (configService: ConfigService) => {
-//         const isProduction = configService.get('NODE_ENV') === 'production';
-//         const redisUrl = configService.get('REDIS_URL');
+@Module({
+  imports: [
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const isProduction = configService.get('NODE_ENV') === 'production';
+        const redisUrl = configService.get('REDIS_URL');
 
-//         // if (isProduction && redisUrl) {
-//         //   return {
-//         //     store: redisStore,
-//         //     url: redisUrl,
-//         //     ttl: configService.get('REDIS_TTL', 300) * 1000, // 5 хвилин
-//         //   };
-//         // }
+        // if (isProduction && redisUrl) {
+        //   return {
+        //     store: redisStore,
+        //     url: redisUrl,
+        //     ttl: configService.get('REDIS_TTL', 300) * 1000, // 5 хвилин
+        //   };
+        // }
 
-//         return {
-//           ttl: configService.get('CACHE_TTL', 300) * 1000, // 5 хвилин
-//           max: configService.get('CACHE_MAX_ITEMS', 100), // максимум 100 елементів
-//         };
-//       },
-//       inject: [ConfigService],
-//       isGlobal: true,
-//     }),
-//   ],
-//   exports: [CacheModule],
-// })
+        return {
+          ttl: configService.get('CACHE_TTL', 300) * 1000, // 5 хвилин
+          max: configService.get('CACHE_MAX_ITEMS', 100), // максимум 100 елементів
+        };
+      },
+      inject: [ConfigService],
+      isGlobal: true,
+    }),
+  ],
+  exports: [CacheModule],
+})
 export class RedisModule {}
